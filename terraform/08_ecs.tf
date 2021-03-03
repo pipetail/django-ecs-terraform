1e3710bc74bb034a2bd60070c61486c0d3a50f43
Marek Bartík <bartimar6@gmail.com>
2021-03-03 21:39:19 +0100

    fix: ignore_changes in ECS task_defs

--- terraform/08_ecs.tf
+++ terraform/08_ecs.tf
@@ -3,11 +3,11 @@ resource "aws_ecs_cluster" "production" {
 }
 
 resource "aws_launch_configuration" "ecs" {
-  name                        = "${var.ecs_cluster_name}-cluster"
-  image_id                    = lookup(var.amis, var.region)
-  instance_type               = var.instance_type
-  security_groups             = [aws_security_group.ecs.id]
-  iam_instance_profile        = aws_iam_instance_profile.ecs.name
+  name                 = "${var.ecs_cluster_name}-cluster"
+  image_id             = lookup(var.amis, var.region)
+  instance_type        = var.instance_type
+  security_groups      = [aws_security_group.ecs.id]
+  iam_instance_profile = aws_iam_instance_profile.ecs.name
   #key_name                    = aws_key_pair.production.key_name
   associate_public_ip_address = true
   user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}-cluster' > /etc/ecs/ecs.config"
@@ -72,6 +72,9 @@ resource "aws_ecs_service" "production" {
   desired_count   = var.app_count
   depends_on      = [aws_alb_listener.ecs-alb-http-listener, aws_iam_role_policy.ecs-service-role-policy, null_resource.build]
 
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
 
   load_balancer {
     target_group_arn = aws_alb_target_group.default-target-group.arn
@@ -84,8 +87,11 @@ resource "aws_ecs_service" "migrate" {
   name            = "${var.ecs_cluster_name}-migrate"
   cluster         = aws_ecs_cluster.production.id
   task_definition = aws_ecs_task_definition.migrate.arn
-  #iam_role        = aws_iam_role.ecs-service-role.arn
-  desired_count   = 1
-  depends_on      = [null_resource.build]
+  desired_count = 1
+  depends_on    = [null_resource.build]
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
 }
 
