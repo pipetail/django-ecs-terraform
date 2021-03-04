63763b2f59174b44ec8ff78205c300e7cf75cd82
Marek Bartík <bartimar6@gmail.com>
2021-03-04 11:54:17 +0100

    fix: ENVIRONMENT->ENV

--- app/hello_django/views.py
+++ app/hello_django/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 import os
 
 def index(request):
-    return HttpResponse("Hello world. You're now viewing the index on " + request.META['HTTP_HOST'] + " in ENVIRONMENT=" + os.environ['ENVIRONMENT'])
+    return HttpResponse("Hello world. You're now viewing the index on " + request.META['HTTP_HOST'] + " in ENVIRONMENT=" + os.environ['ENV'])
