6704f21f9144c16b3edf0340f51e2514fb6f565c
Marek Bartík <bartimar6@gmail.com>
2021-03-10 11:22:25 +0100

    fix: production dynamodb locking

--- terraform/production/provider.tf
+++ terraform/production/provider.tf
@@ -4,9 +4,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "pipetail-django-ecs-terraform-poc"
-    key     = "infra"
-    region  = "eu-west-1"
-    encrypt = true
+    bucket         = "pipetail-django-ecs-terraform-poc"
+    key            = "infra"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-backend"
+    encrypt        = true
   }
 }
